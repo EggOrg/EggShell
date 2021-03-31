17a892a34a76ef914b908e4bd93d02ebbb77bf13
Steve <studiouifxdesignersandcoders@gmail.com>
2021-03-31 07:37:56 -0400

    Clicking E button switches to Windows shell.

--- EggShell/Form1.Designer.cs
+++ EggShell/Form1.Designer.cs
@@ -43,6 +43,7 @@ namespace EggShell
             this.label1.Size = new System.Drawing.Size(22, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "E";
+            this.label1.Click += new System.EventHandler(this.CloseShell);
             // 
             // panel1
             // 
--- EggShell/Form1.cs
+++ EggShell/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,16 +13,30 @@ namespace EggShell
 {
     public partial class Form1 : Form
     {
+        [DllImport("user32.dll")]
+        private static extern int FindWindow(string className, string windowText);
+        [DllImport("user32.dll")]
+        private static extern int ShowWindow(int hwnd, int command);
+        private const int SW_HIDE = 0;
+        private const int SW_SHOW = 1;
         public Form1()
         {
             InitializeComponent();
         }
         public void Init(object sender, EventArgs e)
         {
+            int hwnd = FindWindow("Shell_TrayWnd", "");
+            ShowWindow(hwnd, SW_HIDE);
             panel1.Size = this.Size;
             SetStyle(ControlStyles.SupportsTransparentBackColor, true);
             this.BackColor = Color.Transparent;
             this.Location = new Point(this.Location.X, Screen.PrimaryScreen.Bounds.Height - this.Size.Height);
+        }
+        public void CloseShell(object sender, EventArgs e)
+        {
+            int hwnd = FindWindow("Shell_TrayWnd", "");
+            ShowWindow(hwnd, SW_SHOW);
+            Application.Exit();
         }
     }
 }

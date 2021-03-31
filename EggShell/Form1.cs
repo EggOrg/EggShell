ef2c8d1dee3f55ff6fbdaea79d8d2b64305d19ed
Steve <studiouifxdesignersandcoders@gmail.com>
2021-03-31 07:34:27 -0400

    Add project files.

---
+++ EggShell/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EggShell
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void Init(object sender, EventArgs e)
+        {
+            panel1.Size = this.Size;
+            SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            this.BackColor = Color.Transparent;
+            this.Location = new Point(this.Location.X, Screen.PrimaryScreen.Bounds.Height - this.Size.Height);
+        }
+    }
+}
